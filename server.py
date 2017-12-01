8a2067f7bb1ce4bc4a9d2c9de41ce984f6b83b96
rng <mancng@gmail.com>
2017-11-30 18:39:34 -0800

    added what's my name assignment

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+#-------------------------------------------
+# Landing Page Assignment
+#-------------------------------------------
+
 from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 
@@ -13,11 +17,24 @@ def ninjas():
 def dojo():
     return render_template('dojos.html')
 
-@app.route('/process', methods=["POST"])
-def dojos():
-    print "this is a post route"
-    return redirect('/')
-
-
-
 app.run(debug=True)
+
+
+#-------------------------------------------
+# What's My Name Assignment
+#-------------------------------------------
+
+# from flask import Flask, render_template, redirect, request
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('dojos.html')
+
+# @app.route('/process', methods=["POST"])
+# def dojos():
+#     my_data = request.form['name']
+#     print my_data
+#     return redirect('/')
+
+# app.run(debug=True)
